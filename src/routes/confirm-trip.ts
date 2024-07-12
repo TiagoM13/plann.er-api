d847d0f4168f8c3b0087f7fc11c4370e8766a5b7
TiagoM13 <thyagomotha2000@gmail.com>
2024-07-12 12:11:15 -0300

    feat: finished api

--- src/routes/confirm-trip.ts
+++ src/routes/confirm-trip.ts
@@ -5,6 +5,8 @@ import { prisma } from "../lib/prisma";
 import { getMailClient } from "../lib/mailer";
 import { dayjs } from "../lib/dayjs";
 import nodemailer from 'nodemailer';
+import { ClientError } from "../errors/client-error";
+import { env } from "../env";
 
 export async function confirmTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get('/trips/:tripId/confirm', {
@@ -30,11 +32,11 @@ export async function confirmTrip(app: FastifyInstance) {
     })
 
     if (!trip) {
-      throw new Error('Trip not found.')
+      throw new ClientError('Trip not found.')
     }
 
     if (trip.is_confirmed) {
-      return reply.redirect(`http::/localhost:3000/trips/${tripId}`)
+      return reply.redirect(`${env.WEB_BASE_URL}/trips/${tripId}`)
     }
 
     await prisma.trip.update({
@@ -48,15 +50,15 @@ export async function confirmTrip(app: FastifyInstance) {
     const mail = await getMailClient()
 
     await Promise.all(
-      trip.participants.map(async (particpant) => {
-        const confirmationLink = `http://localhost:3333/participants/${particpant.id}/confirm`
+      trip.participants.map(async (participant) => {
+        const confirmationLink = `${env.API_BASE_URL}/participants/${participant.id}/confirm`
 
         const message = await mail.sendMail({
           from: {
             name: 'Equipe plann.er',
             address: 'equipe@plann.er'
           },
-          to: particpant.email,
+          to: participant.email,
           subject: `Confirme sua presença na viagem para ${trip.destination} em ${formattedStartDate}`,
           html: `
             <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
@@ -77,6 +79,6 @@ export async function confirmTrip(app: FastifyInstance) {
       })
     )
 
-    return reply.redirect(`http::/localhost:3000/trips/${tripId}`)
+    return reply.redirect(`${env.WEB_BASE_URL}/trips/${tripId}`)
   })
 }
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,19 @@ import { fastify } from 'fastify'
 import cors from '@fastify/cors'
 import { createTrip } from './routes/create-trip'
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
-import { confirmTrip } from './routes/confim-trip';
+import { confirmTrip } from './routes/confirm-trip';
 import { confirmParticipant } from './routes/confirm-participant';
 import { createActivity } from './routes/create-activity';
 import { getActivities } from './routes/get-activities';
 import { createLink } from './routes/create-link';
 import { getLinks } from './routes/get-links';
+import { getParticipants } from './routes/get-participants';
+import { createInvite } from './routes/create-invite';
+import { updateTrip } from './routes/update-trip';
+import { getTripDetails } from './routes/get-trip-details';
+import { getParticipant } from './routes/get-participant';
+import { errorHandler } from './error-handler';
+import { env } from './env';
 
 const app = fastify()
 
@@ -18,6 +25,8 @@ app.register(cors, {
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
+app.setErrorHandler(errorHandler)
+
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipant)
@@ -25,7 +34,12 @@ app.register(getActivities)
 app.register(createActivity)
 app.register(getLinks)
 app.register(createLink)
+app.register(getParticipants)
+app.register(createInvite)
+app.register(updateTrip)
+app.register(getTripDetails)
+app.register(getParticipant)
 
-app.listen({ port: 3333 }).then(() => {
-  console.log('listening on port http://localhost:3333')
+app.listen({ port: env.PORT }).then(() => {
+  console.log('Server running...')
 })
